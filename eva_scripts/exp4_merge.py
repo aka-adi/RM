873b0df18bf34fb3bdc2a13c57abf7a0dbb6e483
xiwei <2424232508@qq.com>
2026-04-15 21:03:30 +0800

    fix: lab for merge

--- eva_scripts/exp4_merge.py
+++ eva_scripts/exp4_merge.py
@@ -7,16 +7,16 @@ M = 1000000
 
 approaches = ['rabit']
 cardinalities = ['1024']
-rows = 100*M
-radio = [Decimal('0.4')]
+rows = 10*M
+radio = [Decimal('0.8')]
 ranges = 0.35
 workers = 16
-merge_workers_list = [1, 2, 4]
+merge_thread = [4, 8, 16, 32, 64, 10000000]
 group_len = [64]
 total = 2000
 word_size = 32
 
-def range_query(w, mw, a, c, total, queries_ratio, rows, e, group_len, q_range, v, out_dir):
+def range_query(w, mh, a, c, total, queries_ratio, rows, e, group_len, q_range, v, out_dir):
     index_path = "BM_uniform_" + str(int(rows / M)) + "M_" + str(c) + "_"
     if (e == "AE"):
         index_path = index_path + "AE_" + str(group_len) + "_" + str(word_size)
@@ -25,10 +25,10 @@ def range_query(w, mw, a, c, total, queries_ratio, rows, e, group_len, q_range, 
 
     group_path = "BM_" + str(int(rows / M)) + "M_" + str(c) + "_GE_" + str(group_len) + "_" + str(word_size)
 
-    cmd = './build/nicolas --mode range --approach {} --workers {} --merge-threads {} --number-of-queries {} --number-of-udis {} --cardinality {} --index-path {} --number-of-rows {} --rows-per-seg 100000 --verbose {} --encoding-scheme {} --group-path {} --GE-group-len {} --RQ-length {}'.\
-            format(a, w, mw, int(total*queries_ratio), int(total*(1-queries_ratio)), c, index_path, rows, v, e, group_path, group_len, q_range)
+    cmd = './build/nicolas --mode range --approach {} --workers {} --merge-threads 4 --merge-threshold {} --number-of-queries {} --number-of-udis {} --cardinality {} --index-path {} --number-of-rows {} --rows-per-seg 100000 --verbose {} --encoding-scheme {} --group-path {} --GE-group-len {} --RQ-length {}'.\
+            format(a, w, mh, int(total*queries_ratio), int(total*(1-queries_ratio)), c, index_path, rows, v, e, group_path, group_len, q_range)
 
-    output_file = out_dir + '/eva_{}_{}_{}M_c_{}_w_{}_mw_{}_ratio_{}_range_{}_GL_{}.rawdata'.format(a, 'latency' if (v == 'true') else 'throughput', int(rows / M), c, w, mw, 1 - queries_ratio, q_range, group_len)
+    output_file = out_dir + '/eva_{}_{}_{}M_c_{}_w_{}_mh_{}_ratio_{}_range_{}_GL_{}.rawdata'.format(a, 'latency' if (v == 'true') else 'throughput', int(rows / M), c, w, mh, 1 - queries_ratio, q_range, group_len)
 
     print(cmd)
     result = subprocess.run(shlex.split(cmd), capture_output=True, text=True)
@@ -53,8 +53,8 @@ def main():
             for q in radio:
                 r = int(ranges * float(c))
                 for l in group_len:
-                    for mw in merge_workers_list:
-                        range_query(workers, mw, a, c, total, q, rows, e, l, r, 'true', eva_directory_name)
+                    for mh in merge_thread:
+                        range_query(workers, mh, a, c, total, q, rows, e, l, r, 'true', eva_directory_name)
 
 if __name__ == '__main__':
     main()
